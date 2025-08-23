f47c7c5d3dcc5bd6bc0c018254602a8405d605ae
Lorenaruli <lorenzanaruli@gmail.com>
2025-08-23 19:18:37 +0200

    primo commit

--- src/main/java/it/uniroma2/eu/bookcycle/App.java
+++ src/main/java/it/uniroma2/eu/bookcycle/App.java
@@ -1,16 +1,29 @@
 package it.uniroma2.eu.bookcycle;
 
 import it.uniroma2.eu.bookcycle.controller.grafica.guiComune.ViewPath;
+import it.uniroma2.eu.bookcycle.model.dao.FactoryDao;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class App extends Application {
+
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
     @Override
     public void start(Stage stage) throws Exception {
+        InputStream input = FactoryDao.class.getClassLoader().getResourceAsStream("proprieta.properties");
+        logger.log(Level.INFO, "Trovato file: {0}", (input != null));
+
         // Per la Gui2 Partire con ViewPath.SCHERMATA_ACCESSO_VIEW;
+        try {
             String path = ViewPath.REGISTRAZIONE_VIEW;
             FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
             Parent root = loader.load();
@@ -18,7 +31,10 @@ public class App extends Application {
             stage.setScene(scene);
             stage.setTitle("bookCycle");
             stage.show();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Errore nel caricamento della view", e);
         }
+    }
 
     }
 
--- src/main/java/it/uniroma2/eu/bookcycle/Main.java
+++ src/main/java/it/uniroma2/eu/bookcycle/Main.java
@@ -1,26 +1,17 @@
 package it.uniroma2.eu.bookcycle;
 
 import it.uniroma2.eu.bookcycle.model.dao.FactoryDao;
+import javafx.application.Application;
 
 import java.io.InputStream;
 
 import static javafx.application.Application.launch;
+
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
-
-    public void main(){
-            InputStream input = FactoryDao.class.getClassLoader().getResourceAsStream("proprieta.properties");
-            logger.info("Trovato file: " + (input != null));
-
-            launch(App.class);
-        }
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
     }
-
-
-
-
-
-
+}
